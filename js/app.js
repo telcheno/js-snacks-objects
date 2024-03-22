324408c82434f03ae1e4e5d55480d7d18d92c197
telmo-checa <telmocheca1@gmail.com>
2024-03-22 18:42:57 +0100

    ADD: index.html e app.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log("Js Objects")
+
+/*Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e
+alimentazione (benzina, diesel, gpl, elettrico, metano).
+Dividi le automobili in 3 array separati: nel primo array solo le auto a
+benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+Infine stampa separatamente i 3 array. */
+
