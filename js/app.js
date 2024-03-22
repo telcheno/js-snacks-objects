d53a1aa19ce1ccf7fe0bf2ee39ed26d016357802
telmo-checa <telmocheca1@gmail.com>
2024-03-22 20:48:04 +0100

    ADD. array con le automobili

--- js/app.js
+++ js/app.js
@@ -7,3 +7,58 @@ Dividi le automobili in 3 array separati: nel primo array solo le auto a
 benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
 Infine stampa separatamente i 3 array. */
 
+//creo array con 10 
+const automobile =  [
+  {
+  marca: "fiat",
+  modello: "punto",
+  alimentazione: "benzina"
+  },
+  {
+  marca: "jeep",
+  modello: "renegade",
+  alimentazione: "benzina"
+  },
+  {
+  marca: "opel",
+  modello: "mocca",
+  alimentazione: "elettrico"
+  },
+  {
+  marca: "dacia",
+  modello: "sandero",
+  alimentazione: "gpl"
+  },
+  {
+  marca: "mercedes",
+  modello: "clase a",
+  alimentazione: "disel"
+  },
+  {
+  marca: "fiat",
+  modello: "panda",
+  alimentazione: "disel"
+  },
+  {
+  marca: "citroen",
+  modello: "c1",
+  alimentazione: "metano"
+  },
+  {
+  marca: "peuteoge",
+  modello: "308",
+  alimentazione: "elettrico"
+  },
+  {
+  marca: "renault",
+  modello: "clio",
+  alimentazione: "gpl"
+  },
+  {
+  marca: "volkswagen",
+  modello: "tiguan",
+  alimentazione: "metano"
+  },
+]
+typeof automobile;
+console.log(automobile);
